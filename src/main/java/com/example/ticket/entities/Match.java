126150f7103cc3950930777d7dae7d1c631dba92
Asmaa Tounsi <83168701+asmaatou@users.noreply.github.com>
2022-12-21 18:47:59 +0100

    2

--- src/main/java/com/example/ticket/entities/Match.java
+++ src/main/java/com/example/ticket/entities/Match.java
@@ -6,16 +6,21 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
+import java.util.List;
+
 @Entity
 @Data @NoArgsConstructor @AllArgsConstructor
 public class Match {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true)
     private String reference;
     @Temporal(value = TemporalType.TIMESTAMP)
     private Date date;
     private String place;
     private String firstTeam;
     private String secondTeam;
+    @OneToMany(mappedBy = "match")
+    private List<Ticket> tickets;
 
 }
--- src/main/java/com/example/ticket/entities/Ticket.java
+++ src/main/java/com/example/ticket/entities/Ticket.java
@@ -1,10 +1,7 @@
 package com.example.ticket.entities;
 
 import com.example.ticket.enums.Status;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,7 +11,10 @@ import lombok.NoArgsConstructor;
 public class Ticket {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true)
     private String reference;
     private double price;
     private Status status;
+    @ManyToOne
+    private Match match;
 }
