99b0e493b102d637d56d385bad0761329de69f52
Asmaa Tounsi <83168701+asmaatou@users.noreply.github.com>
2022-12-21 18:39:43 +0100

    1

--- src/main/java/com/example/ticket/entities/Match.java
+++ src/main/java/com/example/ticket/entities/Match.java
@@ -1,4 +1,21 @@
 package com.example.ticket.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor
 public class Match {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String reference;
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date date;
+    private String place;
+    private String firstTeam;
+    private String secondTeam;
+
 }
